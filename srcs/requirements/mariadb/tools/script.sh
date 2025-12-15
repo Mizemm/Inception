864caa02d85de1e19e004e189cfb0ea13f78dc11
mizem <amineshady6@gmail.com>
2025-12-15 22:12:47 +0100

    last

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# service mariadb start
-
-# # service mysql start
-# mysql -h localhost -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-# mysql -h localhost -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-# mysql -h localhost -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
-# # mysql -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD -e "ALTER USER '$MYSQL_ROOT_PASSWORD'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-# mysql -h localhost -e "FLUSH PRIVILEGES;"
-# mysqladmin -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD shutdown
-
-
-# exec mysqld --console
-
 service mariadb start
 # sleep 3
 
